5636fd7c99059cf4ea039ac8ac08ce9af16d31fe
Ylongs <ysl4936@126.com>
2024-01-13 18:24:32 +0800

    [feature]: 完成复制器查询功能
    todo: 复制器内容翻译成中文

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Context, Schema, Session } from 'koishi';
 import cheerio from 'cheerio';
 import {} from 'koishi-plugin-puppeteer';
+import path from 'path'
+import fs from 'fs'
+
 
 export const name = 'apexMap';
 export const inject = ['puppeteer'] as const;
@@ -36,127 +39,176 @@ function translateMapName(englishMapName) {
   return mapNameTranslations[englishMapName] || englishMapName;
 }
 
-export function apply(ctx: Context) {
+// apexmap命令的实现
+function apexmap(ctx: Context) {
   ctx.command('apexmap', '获取Apex Legends地图信息')
-    .action(async ({ session }) => {
-      let page = await ctx.puppeteer.page();
+  .action(async ({ session }) => {
+    let page = await ctx.puppeteer.page();
 
-      // 打开 Apex Legends 混合模式的地图表
-      await page.goto('https://apexlegendsstatus.com/current-map/ltm/pubs', { waitUntil: 'networkidle0' });
+    // 打开 Apex Legends 混合模式的地图表
+    await page.goto('https://apexlegendsstatus.com/current-map/ltm/pubs', { waitUntil: 'networkidle0' });
 
-      // 获取网页内容
-      let content = await page.content();
-      await page.close();
+    // 获取网页内容
+    let content = await page.content();
+    await page.close();
 
-      // 使用 Cheerio 解析 HTML
-      let $ = cheerio.load(content);
+    // 使用 Cheerio 解析 HTML
+    let $ = cheerio.load(content);
 
-      // 从页面中提取地图和玩法的映射关系
-      const maps = [];
-      $('div.row > div').each((index, element) => {
-        const mapElement = $(element).find('h3');
-        // 检查是否找到了 h3 元素并且文本中包含 ' - '
-        if (mapElement.length === 0 || !mapElement.text().includes(' - ')) {
-          return; // 跳过当前元素
-        }
-      
-        const mapNameMode = mapElement.text().split(' - ');
-        const mapName = mapNameMode[0].trim();
-        const mode = mapNameMode[1].trim();
-        maps.push({ mapName, mode });
-      });
-      
-
-      // 在这里，maps 变量包含了地图和玩法的映射关系
-      // 创建一个映射对象用于存储去重后的地图和模式
-      const uniqueMaps = {};
-
-      // 遍历 maps 数组，并将不重复的地图和模式添加到 uniqueMaps 对象中
-      maps.forEach(map => {
-        if (!uniqueMaps[map.mapName]) {
-          uniqueMaps[map.mapName] = map.mode;
-        }
-      });
-
-      // 转换 uniqueMaps 对象为数组格式
-      const uniqueMapsArray = Object.entries(uniqueMaps).map(([mapName, mode]) => {
-        return { mapName, mode };
-      });
-
-      // 将 uniqueMapsArray 转换为键值对映射对象
-      const mapModeMappings = uniqueMapsArray.reduce((acc, cur) => {
-        acc[cur.mapName] = cur.mode;
-        return acc;
-      }, {});
-
-      page = await ctx.puppeteer.page();
-      // 打开 Apex Legends 当前地图网站
-      await page.goto('https://apexlegendsstatus.com/current-map', { waitUntil: 'networkidle0' });
-
-      await page.waitForFunction(
-        () => {
-          // 确保所有计时器加载完成
-          const timer = document.querySelector('div[onclick*="/current-map/battle_royale/pubs"] h2[id="timer"]');
-          const timerRanked = document.querySelector('div[onclick*="/current-map/battle_royale/ranked"] h2[id="timer-ranked"]');
-          const timerLtm = document.querySelector('div[onclick*="/current-map/ltm/pubs"] h2[id="timer-ltm"]');
-          return timer && timer.textContent !== "Loading..."
-              && timerRanked && timerRanked.textContent !== "Loading..."
-              && timerLtm && timerLtm.textContent !== "Loading...";
-        },
-        { timeout: 20000 }
-      );
-
-      content = await page.content();
-      await page.close();
-
-      $ = cheerio.load(content);
-      
-      // 使用映射表将地图名称转换为玩法和中文名称
-      function getMapModeAndChineseName(englishMapName) {
-        const mode = mapModeMappings[englishMapName] || '未知模式';
-        const chineseMapName = mapNameTranslations[englishMapName] || englishMapName;
-        return `${translateMapName(mode)}: ${chineseMapName}`;
-      }
-
-
-      function extractInfo(selector: string, timerSelectorSuffix = '') {
-        const div = $(selector);
-        let mapName = div.find('h1[style="font-weight: 600; margin-bottom: 5px;"]').text().trim();
-        const timerSelector = `h2[id="timer${timerSelectorSuffix}"]`;
-        const timeRemaining = div.find(timerSelector).text().trim();
-        let nextMapName = div.find('h5').eq(1).find('b').text().trim(); // 获取下一个地图的名称
-    
-        // 对匹配模式和排位模式去除“: ”及其前面的内容
-        if (timerSelectorSuffix !== '-ltm') {
-          const colonIndex = mapName.indexOf(': ');
-          if (colonIndex !== -1) {
-              mapName = mapName.substring(colonIndex + 2);
-          }
-          // 将地图名称翻译成中文（如果有对应的翻译）
-          mapName = translateMapName(mapName);
-          nextMapName = translateMapName(nextMapName);
-          return `当前地图: ${mapName}\n剩余时间: ${timeRemaining}\n下个地图: ${nextMapName}`;
-        }
-        else{
-          // 获取并处理当前地图信息
-          let [currentMode, currentMap] = mapName.split(': ');
-          currentMode = translateMapName(currentMode);
-          currentMap = translateMapName(currentMap);
-
-          // 获取并处理下一个地图信息
-          nextMapName = getMapModeAndChineseName(nextMapName); // 转换成“玩法: 地图名”
-          let [nextMode, nextMap] = nextMapName.split(': ');
-          nextMode = translateMapName(nextMode);
-          nextMap = translateMapName(nextMap);
-          return `当前地图: ${currentMode}: ${currentMap}\n剩余时间: ${timeRemaining}\n下个地图: ${nextMode}: ${nextMap}`;
-        }
+    // 从页面中提取地图和玩法的映射关系
+    const maps = [];
+    $('div.row > div').each((index, element) => {
+      const mapElement = $(element).find('h3');
+      // 检查是否找到了 h3 元素并且文本中包含 ' - '
+      if (mapElement.length === 0 || !mapElement.text().includes(' - ')) {
+        return; // 跳过当前元素
       }
     
-
-      const pubsInfo = extractInfo('div[onclick*="/current-map/battle_royale/pubs"]');
-      const rankedInfo = extractInfo('div[onclick*="/current-map/battle_royale/ranked"]', '-ranked');
-      const ltmInfo = extractInfo('div[onclick*="/current-map/ltm/pubs"]', '-ltm');
-
-      return `匹配模式:\n${pubsInfo}\n\n排位模式:\n${rankedInfo}\n\n混合模式:\n${ltmInfo}`;
+      const mapNameMode = mapElement.text().split(' - ');
+      const mapName = mapNameMode[0].trim();
+      const mode = mapNameMode[1].trim();
+      maps.push({ mapName, mode });
     });
+    
+
+    // 在这里，maps 变量包含了地图和玩法的映射关系
+    // 创建一个映射对象用于存储去重后的地图和模式
+    const uniqueMaps = {};
+
+    // 遍历 maps 数组，并将不重复的地图和模式添加到 uniqueMaps 对象中
+    maps.forEach(map => {
+      if (!uniqueMaps[map.mapName]) {
+        uniqueMaps[map.mapName] = map.mode;
+      }
+    });
+
+    // 转换 uniqueMaps 对象为数组格式
+    const uniqueMapsArray = Object.entries(uniqueMaps).map(([mapName, mode]) => {
+      return { mapName, mode };
+    });
+
+    // 将 uniqueMapsArray 转换为键值对映射对象
+    const mapModeMappings = uniqueMapsArray.reduce((acc, cur) => {
+      acc[cur.mapName] = cur.mode;
+      return acc;
+    }, {});
+
+    page = await ctx.puppeteer.page();
+    // 打开 Apex Legends 当前地图网站
+    await page.goto('https://apexlegendsstatus.com/current-map', { waitUntil: 'networkidle0' });
+
+    await page.waitForFunction(
+      () => {
+        // 确保所有计时器加载完成
+        const timer = document.querySelector('div[onclick*="/current-map/battle_royale/pubs"] h2[id="timer"]');
+        const timerRanked = document.querySelector('div[onclick*="/current-map/battle_royale/ranked"] h2[id="timer-ranked"]');
+        const timerLtm = document.querySelector('div[onclick*="/current-map/ltm/pubs"] h2[id="timer-ltm"]');
+        return timer && timer.textContent !== "Loading..."
+            && timerRanked && timerRanked.textContent !== "Loading..."
+            && timerLtm && timerLtm.textContent !== "Loading...";
+      },
+      { timeout: 20000 }
+    );
+
+    content = await page.content();
+    await page.close();
+
+    $ = cheerio.load(content);
+    
+    // 使用映射表将地图名称转换为玩法和中文名称
+    function getMapModeAndChineseName(englishMapName) {
+      const mode = mapModeMappings[englishMapName] || '未知模式';
+      const chineseMapName = mapNameTranslations[englishMapName] || englishMapName;
+      return `${translateMapName(mode)}: ${chineseMapName}`;
+    }
+
+
+    function extractInfo(selector: string, timerSelectorSuffix = '') {
+      const div = $(selector);
+      let mapName = div.find('h1[style="font-weight: 600; margin-bottom: 5px;"]').text().trim();
+      const timerSelector = `h2[id="timer${timerSelectorSuffix}"]`;
+      const timeRemaining = div.find(timerSelector).text().trim();
+      let nextMapName = div.find('h5').eq(1).find('b').text().trim(); // 获取下一个地图的名称
+  
+      // 对匹配模式和排位模式去除“: ”及其前面的内容
+      if (timerSelectorSuffix !== '-ltm') {
+        const colonIndex = mapName.indexOf(': ');
+        if (colonIndex !== -1) {
+            mapName = mapName.substring(colonIndex + 2);
+        }
+        // 将地图名称翻译成中文（如果有对应的翻译）
+        mapName = translateMapName(mapName);
+        nextMapName = translateMapName(nextMapName);
+        return `当前地图: ${mapName}\n剩余时间: ${timeRemaining}\n下个地图: ${nextMapName}`;
+      }
+      else{
+        // 获取并处理当前地图信息
+        let [currentMode, currentMap] = mapName.split(': ');
+        currentMode = translateMapName(currentMode);
+        currentMap = translateMapName(currentMap);
+
+        // 获取并处理下一个地图信息
+        nextMapName = getMapModeAndChineseName(nextMapName); // 转换成“玩法: 地图名”
+        let [nextMode, nextMap] = nextMapName.split(': ');
+        nextMode = translateMapName(nextMode);
+        nextMap = translateMapName(nextMap);
+        return `当前地图: ${currentMode}: ${currentMap}\n剩余时间: ${timeRemaining}\n下个地图: ${nextMode}: ${nextMap}`;
+      }
+    }
+  
+
+    const pubsInfo = extractInfo('div[onclick*="/current-map/battle_royale/pubs"]');
+    const rankedInfo = extractInfo('div[onclick*="/current-map/battle_royale/ranked"]', '-ranked');
+    const ltmInfo = extractInfo('div[onclick*="/current-map/ltm/pubs"]', '-ltm');
+
+    return `匹配模式:\n${pubsInfo}\n\n排位模式:\n${rankedInfo}\n\n混合模式:\n${ltmInfo}`;
+  });
+}
+
+// apexcraft命令的实现
+function apexcraft(ctx: Context) {
+  ctx.command('apexcraft', '查询Apex Legends的复制器信息')
+  .action(async ({ session }) => {
+    let page = await ctx.puppeteer.page();
+
+    // 打开复制器工艺旋转的网页
+    await page.goto('https://apexlegendsstatus.com/crafting-rotation', { waitUntil: 'networkidle0' });
+
+    // 定位到复制器内容
+    const selector = 'div.row > div.legpickrate > div.row';
+
+    // 等待元素加载
+    await page.waitForSelector(selector);
+
+    // 截取指定元素的截图
+    const screenshotsDir = path.join(__dirname, 'screenshots');
+    fs.mkdirSync(screenshotsDir, { recursive: true });
+    const screenshotPath = path.join(screenshotsDir, "Replicators crafting rotation.png");
+    const element = await page.$(selector);
+    const bounding_box = await element.boundingBox();
+    const screenshot = await element.screenshot({
+      clip: {
+        x: bounding_box.x,
+        y: bounding_box.y,
+        width: Math.min(bounding_box.width, page.viewport().width),
+        height: Math.min(bounding_box.height, page.viewport().height)
+      },
+      path: screenshotPath
+    });
+
+    // 关闭页面
+    await page.close();
+
+    // 将截图文件发送给用户
+    const imageBuffer = fs.readFileSync(screenshotPath);
+    const imageBase64 = imageBuffer.toString('base64');
+    const dataUri = 'data:image/png;base64,' + imageBase64;
+
+    return `<image url="${dataUri}"/>`;
+  });
+}
+
+export function apply(ctx: Context) {
+  apexmap(ctx);
+  apexcraft(ctx);
 }
